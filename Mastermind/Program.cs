4e8af7ec002415298439c64d094324b1b1e9e0f1
ArleenMBell <44973242+ArleenMBell@users.noreply.github.com>
2019-10-17 13:41:58 -0400

    Mastermind the commit

---
+++ Mastermind/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace Mastermind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+
+            var result = string.Empty;
+            var youWin = false;
+            var tryNum = 0;
+
+            var first = rand.Next(1, 6);
+            var second = rand.Next(1, 6);
+            var third = rand.Next(1, 6);
+            var fourth = rand.Next(1, 6);
+
+            var gameNumber = $"{first}{second}{third}{fourth}".ToCharArray();
+
+
+            while (tryNum < 10)
+            {
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input) || input.Length < 4)
+                {
+                    ++tryNum;
+                    continue;
+                }
+
+                var inputChars = input.ToCharArray();
+
+                for (var index = 0; index < gameNumber.Length; ++index)
+                {
+                    if (gameNumber[index] == inputChars[index])
+                    {
+                        result += "+";
+                    }
+                    else
+                    {
+                        if (gameNumber.Contains(inputChars[index]))
+                        {
+                            result += "-";
+                        }
+                        else
+                        {
+                            result += " ";
+                        }
+                    }
+                }
+
+                Console.WriteLine(result);
+
+                if (result == "++++")
+                {
+                    youWin = true;
+                    break;
+                }
+
+                ++tryNum;
+            }
+
+            Console.WriteLine(youWin ? "You Win!!!" : "You lose!!!");
+
+
+            Console.ReadKey();
+        }
+    }
+}
